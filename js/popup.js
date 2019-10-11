ff04d845fda78fe7b17b42d718c88e9be416b2fb
olyamosunova <54084142+olyamosunova@users.noreply.github.com>
2019-10-11 21:32:46 +0300

    js

---
+++ js/popup.js
@@ -0,0 +1,37 @@
+var link = document.querySelector(".feedback-link");
+var popup = document.querySelector(".feedback");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var userName = popup.querySelector("[name=user-name]");
+var userEmail = popup.querySelector("[name=user-email]");
+var userComment = popup.querySelector("[name=comment]");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!userName.value || !userEmail.value || !userComment.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
