fd9f7005b1e3a65ed4c5d7d94030719ab080104a
olyamosunova <54084142+olyamosunova@users.noreply.github.com>
2019-10-15 21:16:24 +0300

    правки

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,5 @@
 var link = document.querySelector(".feedback-link");
+var overlay = document.querySelector(".overlay");
 var popup = document.querySelector(".feedback");
 var close = popup.querySelector(".modal-close");
 var form = popup.querySelector("form");
@@ -8,12 +9,12 @@ var userComment = popup.querySelector("[name=comment]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
+  overlay.classList.add("modal-show");
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("modal-show");
+  overlay.classList.remove("modal-show");
   popup.classList.remove("modal-error");
 });
 
@@ -28,9 +29,9 @@ form.addEventListener("submit", function (evt) {
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
+    if (overlay.classList.contains("modal-show")) {
       evt.preventDefault();
-      popup.classList.remove("modal-show");
+      overlay.classList.remove("modal-show");
       popup.classList.remove("modal-error");
     }
   }
